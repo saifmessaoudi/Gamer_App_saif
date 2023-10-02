7f18f3a3f1c137e92ae80ead53e826c920f7b661
saifmessaoudi <saifmessaoudi99@gmail.com>
2023-10-03 00:30:32 +0100

    Gamer App init

---
+++ app/src/main/java/com/example/saif_messaoudi_game_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.saif_messaoudi_game_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "saif_messaoudi_Game_App"
+include(":app")
+ 
